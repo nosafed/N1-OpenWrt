734b7064da8b5bf2010aa95d269a51777db6d9d7
nosafed <78470602+nosafed@users.noreply.github.com>
2025-05-07 16:52:24 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -25,9 +25,9 @@ git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-
 git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 #添加自定义的软件包源
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
-# Remove packages
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-theme-edge
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-lucky
+Remove packages
 #删除lean库中的插件，使用自定义源中的包。
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/themes/luci-theme-argon
